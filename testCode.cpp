32c91ba4dfbe0462945fa39040234e8456df4ca2
tiel <cgp138138@gmail.com>
2025-05-12 19:32:22 -0600

    Add test file to be used for printouts

---
+++ testCode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "LinkedList.h"
+
+int main()
+{
+    // Declare a LinkedList to demonstrate that there are no compile/runtime errors
+    LinkedList ll;
+    std::cout << "Ran without issue.\n";
+
+}
